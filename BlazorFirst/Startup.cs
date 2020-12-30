4480e67d84f3d85743453079558fe29a90f0966b
seung9721 <seung9721@naver.com>
2020-12-30 12:28:13 +0900

    MatBlazor:error

--- BlazorFirst/Startup.cs
+++ BlazorFirst/Startup.cs
@@ -1,4 +1,5 @@
 using BlazorFirst.Data;
+using MatBlazor;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
@@ -29,6 +30,7 @@ namespace BlazorFirst
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+            services.AddMatBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
